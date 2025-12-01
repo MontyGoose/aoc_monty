9fecc53dadd49dd191c97659f54ce7d3cd9368d2
MontyGoose <mr.andy.parry@gmail.com>
2025-12-01 13:41:03 +0000

    day 1 part 2

--- day1/main.py
+++ day1/main.py
@@ -15,7 +15,6 @@
 #load data from file data.txt
 with open("data.txt", "r") as file:
     data = file.read()
-print(data)
 
 
 
@@ -26,32 +25,53 @@ raw_elements = data.strip().split("\n")
 elements = []
 start = 50
 zero_count = 0
+full_zero_count = 0
 for entry in raw_elements:
-    #print("start", start)
+    #print("current", start)
+    finish = start
     direction = entry[0]
     distance = int(entry[1:])
-    elements.append({"direction": direction, "distance": distance})
+    #print("-- spin", direction, distance)
 
     # if distance is greater taht 100 then we only want to use the tens and units
     if distance > 100:
+        print(entry)
+        full_zero_count += int(str(distance)[0])
+        print("-- pass zero", int(str(distance)[0]))
+        print(" --- --- fcz", full_zero_count)
         distance = int(str(distance)[-2:])
-        print("distance", distance)
+    #    print("-- distance", distance)
 
     if direction == "L":
-        start -= distance
+        finish -= distance
     elif direction == "R":
-        start += distance
+        finish += distance
 
-    if start < 0:
-        start += 100
+    if finish < 0:
+        if start != 0:
+            full_zero_count += 1
+            print("-- passed zero", 1)
+        finish += 100
 
-    if start == 0 or start == 100:
-        start =0
+    if finish == 0 or finish == 100:
+        finish = 0
         zero_count += 1
-        print(f"Visited 0")
+        print(f"-- Visited 0")
 
-    if start > 100:
-        start -= 100
+    if finish > 100:
+        full_zero_count += 1
+        print("-- passs zero", 1)
+        finish -= 100
+    start = finish
 
+print("current", start)
+print("--------------------------------")
+print("stopped on zero", zero_count)
+print("went past zero", full_zero_count)
+print("--------------------------------")
+print("total", zero_count + full_zero_count)
 
-print(zero_count)
+# total is 5876, using 0x434C49434B as the password, what is the password to open the door?
+password = 0x434C49434B * (zero_count + full_zero_count)
+print("password", password) 
+
